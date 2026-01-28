cbe9e68631dc00cb92191303b27873e32c3dc316
KAS <kadyrovas@yandex.ru>
2026-01-28 09:52:45 +0300

    Part 3 updated import lombok

--- src/main/java/ru/skypro/homework/dto/Ad.java
+++ src/main/java/ru/skypro/homework/dto/Ad.java
@@ -1,6 +1,10 @@
 package ru.skypro.homework.dto;
 
-import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 /**
  * DTO (Data Transfer Object) для представления объявления в списке.
